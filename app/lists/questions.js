5bba32e66b2bfa663ca0e091eee8b174d8fd9955
mcaprari <matteo.caprari@gmail.com>
2009-12-17 14:44:28 +0000

    added snippet in question list and search results

--- app/lists/questions.js
+++ app/lists/questions.js
@@ -5,7 +5,7 @@ function(head, req) {
 
 	start({"headers":{"Content-Type" : "text/html; charset=utf-8"}});
 
-	
+	var preview_length=100;	
 	send(Mustache.to_html(templates.head, {title:"questions"}));
 	send(Mustache.to_html(templates.questions.list_head));
 	answers = 0;
@@ -16,6 +16,11 @@ function(head, req) {
 		else if (row.value) {
 			var question = row.value;
 			question.answers = answers;
+			
+			question.question_preview = (question.question.length > preview_length) 
+				? question.question.substring(0,100) + '...'
+				: question.question;
+				
 			send(Mustache.to_html(templates.questions.list_item, question));
 			answers = 0;
 		} 	
