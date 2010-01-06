f97238ba5e899146d031284decc251a89158e7dd
Matteo Caprari <matteo.caprari@gmail.com>
2010-01-06 23:17:03 +0000

    created list that _should_ work with view.questions_index, but doesnt

---
+++ app/lists/questions_index.js
@@ -0,0 +1,45 @@
+/**
+	Used with {@link views.questions}?<strong>descending=true</strong>
+	will show most recent questions first and a count of answers for each question.
+	@name lists.questions
+*/
+function(head, req) {
+// !json templates
+// !code lib/mustache.js
+// !code lib/showdown.js
+
+	start({"headers":{"Content-Type" : "text/html; charset=utf-8"}});
+
+	var preview_length=100;	
+	send(Mustache.to_html(templates.head, {title:"questions"}));
+	send(Mustache.to_html(templates.questions.list_head));
+	answers = 0;
+	var converter = new Showdown.converter();
+
+	while (row = getRow()) {
+		if (row.value==1) {
+			answers++;
+		}	
+		else if (row.value) {
+			var question = row.value;
+			question.answers = answers;
+			
+			
+			question.question_preview = (question.question.length > preview_length) 
+				? converter.makeHtml(question.question.substring(0,100)) + '...'
+				: converter.makeHtml(question.question);
+				
+			
+			
+			question.url_id = encodeURIComponent(question._id).replace(/%22/g,"%5C%22");	
+			
+			send(Mustache.to_html(templates.questions.list_item, question));
+			answers = 0;
+		} 	
+	}
+	
+	send(Mustache.to_html(templates.questions.list_foot));
+	send(Mustache.to_html(templates.foot),{});
+		
+}
+
