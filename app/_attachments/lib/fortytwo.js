0551abfd9afc67b164e3259efa4847e92f02edd1
Matteo Caprari <matteo.caprari@gmail.com>
2010-01-07 18:57:00 +0000

    removed some stale javascript code

--- app/_attachments/lib/fortytwo.js
+++ app/_attachments/lib/fortytwo.js
@@ -1,32 +1,4 @@
 var FortyTwo = {
-	make_id_from_string: function(title) {
-		if (!title) throw({error: 'title is not defined'});
-		return title.toLowerCase()
-			.replace('&','and')
-			.replace(/[\(\)\{\}\[\]!%?*&\\\s]/g,'-')
-			.replace(/-+/g,'-')
-			.replace(/-$/,'')
-			.replace(/"/g,'');
-	},
-	
-	zeropad: function(number, size) {
-		var rt = number + '';
-		while (rt.length < size) {
-			rt = '0' + rt;
-		}
-		return rt;
-	},
-	make_timestamp_from_date: function(date) {
-		return date.getUTCFullYear()
-			+ '-' + FortyTwo.zeropad(date.getUTCMonth(), 2)
-			+ '-' + FortyTwo.zeropad(date.getUTCDate() ,2)
-			+ '-' + FortyTwo.zeropad(date.getUTCHours(), 2)
-			+ FortyTwo.zeropad(date.getUTCMinutes(), 2);
-		
-	},
-	question_details_link: function(question_id) {
-		
-	},
 	escape: function(s) {
 		return s.toString().replace(/[&"<>\\]/g, function(s) {
 			switch(s) {
