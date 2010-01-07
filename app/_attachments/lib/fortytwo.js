c79dac8b06cde44ed30e2ee2bdfe52ac27edd2a9
Matteo Caprari <matteo.caprari@gmail.com>
2010-01-07 18:28:00 +0000

    fixed XSS in detail view. fixed browse link

--- app/_attachments/lib/fortytwo.js
+++ app/_attachments/lib/fortytwo.js
@@ -28,8 +28,8 @@ var FortyTwo = {
 		
 	},
 	escape: function(s) {
-	return s.toString().replace(/[&"<>\\]/g, function(s) {
-		switch(s) {
+		return s.toString().replace(/[&"<>\\]/g, function(s) {
+			switch(s) {
 				case "&": return "&amp;";
 				case "\\": return "\\\\";;
 				case '"': return '\"';;
@@ -38,5 +38,14 @@ var FortyTwo = {
 				default: return s;
 			}
 		});
+	},
+	uuid: function(fn) {
+		$.ajax({
+			url: '/_uuids',
+			dataType: 'json',
+			success: function(data) {
+				fn(data.uuids.shift());
+			}
+		})
 	}
 }
