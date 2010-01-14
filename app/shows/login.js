9683be5264729cbaa888f21945ea7cb06041d802
mcaprari <matteo.caprari@gmail.com>
2010-01-14 14:08:36 +0000

    initial work on abstracting urls

--- app/shows/login.js
+++
@@ -1,8 +0,0 @@
-function(doc, req) {
-	// !json templates
-	// !code lib/mustache.js
-	
-	return Mustache.to_html(templates.app_head, {title:'ask question'})
-		+ Mustache.to_html(templates.login_form)
-	 	+ Mustache.to_html(templates.app_foot);
-}
